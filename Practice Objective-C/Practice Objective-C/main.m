696f80e7509c44aa9fa5fc79a29e9a03cc988cb7
key5771 <ksj57715@hanmail.net>
2020-12-28 18:11:35 +0900

    Create Project

---
+++ Practice Objective-C/Practice Objective-C/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Practice Objective-C
+//
+//  Created by 김기현 on 2020/12/28.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, Objective-C!");
+    }
+    return 0;
+}
