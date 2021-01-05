b7d8f8db5e632d28018b997cde74c527b5ec2cd3
key5771 <ksj57715@hanmail.net>
2021-01-05 15:14:11 +0900

    Create Circle & Rectangle Class

--- Inheritance/Inheritance/main.m
+++ Inheritance/Inheritance/main.m
@@ -7,10 +7,109 @@
 
 #import <Foundation/Foundation.h>
 
+typedef enum {
+    kRedColor,
+    kGreenColor,
+    kBlueColor
+} ShapeColor;
+
+typedef struct {
+    int x, y, width, height;
+} ShapeRect;
+
+NSString *colorName(ShapeColor colorName) {
+    switch (colorName) {
+        case kRedColor:
+            return @"red";
+        case kGreenColor:
+            return @"green";
+        case kBlueColor:
+            return @"blue";
+        default:
+            break;
+    }
+    
+    return @"no clue";
+}
+
+@interface Circle : NSObject {
+    ShapeColor fillColor;
+    ShapeRect bounds;
+}
+
+- (void) setFillColor: (ShapeColor) fillColor;
+- (void) setBounds: (ShapeRect) bounds;
+- (void) draw;
+
+@end // Circle
+
+@interface Rectangle : NSObject {
+    ShapeColor fillColor;
+    ShapeRect bounds;
+}
+
+- (void) setFillColor: (ShapeColor) fillColor;
+- (void) setBounds: (ShapeRect) bounds;
+- (void) draw;
+
+@end // Rectangle
+
+@implementation Circle
+
+- (void) setFillColor:(ShapeColor) c {
+    fillColor = c;
+}
+
+- (void) setBounds:(ShapeRect) b {
+    bounds = b;
+}
+
+- (void) draw {
+    NSLog(@"drawing a circle at (%d, %d, %d, %d) in %@",
+          bounds.x, bounds.y, bounds.width, bounds.height, colorName(fillColor));
+}
+
+@end // Circle
+
+@implementation Rectangle
+
+- (void) setFillColor:(ShapeColor) c {
+    fillColor = c;
+}
+
+- (void) setBounds:(ShapeRect) b {
+    bounds = b;
+}
+
+- (void) draw {
+    NSLog(@"drawing a rectangle at (%d, %d, %d, %d) in %@",
+          bounds.x, bounds.y, bounds.width, bounds.height, colorName(fillColor));
+}
+
+@end // Rectangle
+
+void drawShapes(__strong id shapes[], int count) {
+    for (int i = 0; i < count; i++) {
+        id shape = shapes[i];
+        [shape draw];
+    }
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        id shapes[2];
+        
+        ShapeRect rect0 = {0, 0, 10, 30};
+        shapes[0] = [Circle new];
+        [shapes[0] setBounds: rect0];
+        [shapes[0] setFillColor: kRedColor];
+        
+        ShapeRect rect1 = {10, 10, 10, 30};
+        shapes[1] = [Rectangle new];
+        [shapes[1] setBounds: rect1];
+        [shapes[1] setFillColor: kBlueColor];
+        
+        drawShapes(shapes, 2);
     }
     return 0;
 }
