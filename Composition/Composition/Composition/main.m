9bfc8a755fb5f9375003904bdf2983a3ff1d62e5
key5771 <ksj57715@hanmail.net>
2021-01-12 17:58:16 +0900

    Create setter & getter in Car class(Engine, Tire)

--- Composition/Composition/Composition/main.m
+++ Composition/Composition/Composition/main.m
@@ -34,6 +34,15 @@
     Tire *tires[4];
 }
 
+- (Engine *) engine;
+
+- (void) setEngine: (Engine *) newEngine;
+
+- (Tire *) tireAtIndex: (int) index;
+
+- (void) setTire: (Tire *) tire
+         atIndex: (int) index;
+
 - (void) print;
 
 @end // Car
@@ -53,6 +62,32 @@
     return (self);
 } // init
 
+- (Engine *) engine {
+    return (engine);
+} // engine
+
+- (void) setEngine:(Engine *) newEngine {
+    engine = newEngine;
+} // setEngine
+
+- (void) setTire: (Tire *) tire atIndex: (int) index {
+    if (index < 0 || index > 3) {
+        NSLog(@"bad index (%d) in setTire:atIndex:", index);
+        exit(1);
+    }
+    
+    tires[index] = tire;
+} // setTire:atIndex:
+
+- (Tire *) tireAtIndex: (int) index {
+    if (index < 0 || index > 3) {
+        NSLog(@"bad index (%d) in tireAtIndex:", index);
+        exit(1);
+    }
+    
+    return (tires[index]);
+} // tireAtIndex:
+
 - (void) print {
     NSLog(@"%@", engine);
     
@@ -67,11 +102,20 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        Car *car;
+        Car *car = [Car new];
         
-        car = [Car new];
+        Engine *engine = [Engine new];
+        [car setEngine: engine];
+        
+        int i;
+        for (i = 0; i < 4; i++) {
+            Tire *tire = [Tire new];
+            
+            [car setTire: tire atIndex: i];
+        }
+        
         [car print];
     }
     
     return 0;
-}
+} // main
