b35526dac917dbe4630b5921691c4b358b5435b0
key5771 <ksj57715@hanmail.net>
2021-01-19 16:28:21 +0900

    Create Project

---
+++ MemoryManagement/MemoryManagement/MemoryManagement/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MemoryManagement
+//
+//  Created by 김기현 on 2021/01/19.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
