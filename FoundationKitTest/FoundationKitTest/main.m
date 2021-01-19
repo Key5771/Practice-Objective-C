d3c398a6da1f1abeb5929070f28ba2312dc5af0c
key5771 <ksj57715@hanmail.net>
2021-01-19 16:26:40 +0900

    Fast Enumeration

--- FoundationKitTest/FoundationKitTest/main.m
+++ FoundationKitTest/FoundationKitTest/main.m
@@ -7,22 +7,49 @@
 
 #import <Foundation/Foundation.h>
 
+//int main(int argc, const char * argv[]) {
+//    @autoreleasepool {
+//        NSMutableArray *array;
+//        array = [NSMutableArray array];
+//
+//        NSMutableDictionary *dictionary;
+//        dictionary = [NSMutableDictionary dictionary];
+//
+//        NSNumber *number;
+//        number = [NSNumber numberWithInt: 42];
+//        [array addObject: number];
+//        [dictionary setObject: number forKey: @"Bork"];
+//
+//        NSLog(@"array is %@", array);
+//        NSLog(@"dictionary is %@", dictionary);
+//    }
+//
+//    return 0;
+//}
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSMutableArray *array;
-        array = [NSMutableArray array];
+        NSFileManager *manager;
+        manager = [NSFileManager defaultManager];
         
-        NSMutableDictionary *dictionary;
-        dictionary = [NSMutableDictionary dictionary];
+        NSString *home;
+        home = [@"~" stringByExpandingTildeInPath];
         
-        NSNumber *number;
-        number = [NSNumber numberWithInt: 42];
-        [array addObject: number];
-        [dictionary setObject: number forKey: @"Bork"];
+        NSMutableArray *files;
+        files = [NSMutableArray arrayWithCapacity: 42];
         
-        NSLog(@"array is %@", array);
-        NSLog(@"dictionary is %@", dictionary);
+        for (NSString *filename in [manager enumeratorAtPath: home]) {
+            if ([[filename pathExtension] isEqual: @"jpg"]) {
+                [files addObject: filename];
+            }
+        }
+        
+        for (NSString *filename in files) {
+            NSLog(@"%@", filename);
+        }
+        
+        NSLog(@"return");
     }
-    
     return 0;
 }
