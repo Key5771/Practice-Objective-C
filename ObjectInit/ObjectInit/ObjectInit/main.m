392d30310426cac8db14f78a343d207c3e3ae9df
key5771 <ksj57715@hanmail.net>
2021-01-20 14:26:57 +0900

    Create Project

---
+++ ObjectInit/ObjectInit/ObjectInit/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  ObjectInit
+//
+//  Created by 김기현 on 2021/01/20.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+    }
+    return 0;
+}
