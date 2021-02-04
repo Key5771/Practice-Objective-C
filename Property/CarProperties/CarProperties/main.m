7d40fc2be1b708118cbdeb1010cceb785016f3ac
key5771 <ksj57715@hanmail.net>
2021-02-04 16:49:18 +0900

    Tire 및 Engine을 갖는 Car 생성

--- Property/CarProperties/CarProperties/main.m
+++ Property/CarProperties/CarProperties/main.m
@@ -6,11 +6,33 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Tire.h"
+#import "Engine.h"
+#import "Car.h"
+#import "Slant6.h"
+#import "AllWeatherRadial.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        Car *car = [[Car alloc] init];
+        
+        int i;
+        for (i = 0; i < 4; i++) {
+            AllWeatherRadial *tire;
+            
+            tire = [[AllWeatherRadial alloc] init];
+            [tire setRainHandling: 20 + i];
+            [tire setSnowHandling: 28 + i];
+            NSLog(@"the tire's handling is %.f %.f", [tire rainHandling], [tire snowHandling]);
+            
+            [car setTire: tire atIndex: i];
+        }
+        
+        Engine *engine = [[Slant6 alloc] init];
+        [car setEngine: engine];
+        
+        [car print];
     }
+    
     return 0;
 }
