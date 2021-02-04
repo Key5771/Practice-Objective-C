b194dc47571da477354e67af2248df636c73a538
key5771 <ksj57715@hanmail.net>
2021-02-04 17:04:47 +0900

    AllWeatherRadial 기존 구현부를 @synthesize를 통해 세터/게터 메소드를 사용하지 않도록 수정

--- Property/CarProperties/CarProperties/Tire/AllWeatherRadial.m
+++ Property/CarProperties/CarProperties/Tire/AllWeatherRadial.m
@@ -8,9 +8,14 @@
 #import <Foundation/Foundation.h>
 #import "AllWeatherRadial.h"
 
+// MARK: - Synthesize를 사용한 새로운 코드
 @implementation AllWeatherRadial
 
-- (id) initWithPressure: (float) p treadDepth: (float) td {
+@synthesize rainHandling;
+@synthesize snowHandling;
+
+- (id) initWithPressure: (float) p
+             treadDepth: (float) td {
     if (self = [super initWithPressure: p treadDepth: td]) {
         rainHandling = 23.7;
         snowHandling = 42.5;
@@ -19,29 +24,52 @@
     return (self);
 } // initWithPressure:treadDepth:
 
-- (void) setRainHandling: (float) rh {
-    rainHandling = rh;
-} // setRainHandling
-
-- (float) rainHandling {
-    return (rainHandling);
-} // rainHandling
-
-- (void) setSnowHandling: (float) sh {
-    snowHandling = sh;
-}
-
-- (float) snowHandling {
-    return (snowHandling);
-}
-
 - (NSString *) description {
     NSString *desc;
-    
     desc = [[NSString alloc] initWithFormat: @"AllWeatherRadial: %.1f / %.1f / %.1f / %.1f",
             [self pressure], [self treadDepth], [self rainHandling], [self snowHandling]];
     
     return (desc);
-}
+} // description
 
-@end // AllWeatherRadial
+@end
+
+
+// MARK: - 기존에 사용했던 init 메서드와 세터/게터 메소드
+//@implementation AllWeatherRadial
+//
+//- (id) initWithPressure: (float) p treadDepth: (float) td {
+//    if (self = [super initWithPressure: p treadDepth: td]) {
+//        rainHandling = 23.7;
+//        snowHandling = 42.5;
+//    }
+//
+//    return (self);
+//} // initWithPressure:treadDepth:
+//
+//- (void) setRainHandling: (float) rh {
+//    rainHandling = rh;
+//} // setRainHandling
+//
+//- (float) rainHandling {
+//    return (rainHandling);
+//} // rainHandling
+//
+//- (void) setSnowHandling: (float) sh {
+//    snowHandling = sh;
+//}
+//
+//- (float) snowHandling {
+//    return (snowHandling);
+//}
+//
+//- (NSString *) description {
+//    NSString *desc;
+//
+//    desc = [[NSString alloc] initWithFormat: @"AllWeatherRadial: %.1f / %.1f / %.1f / %.1f",
+//            [self pressure], [self treadDepth], [self rainHandling], [self snowHandling]];
+//
+//    return (desc);
+//}
+//
+//@end // AllWeatherRadial
