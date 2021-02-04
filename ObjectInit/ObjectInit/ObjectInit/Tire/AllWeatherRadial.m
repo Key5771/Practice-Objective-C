6a0ef580573d0c045bb595392029d0188363eb47
key5771 <ksj57715@hanmail.net>
2021-02-04 16:23:07 +0900

    Tire에서 선언한 초기화 메서드를 Tire 클래스를 상속받는 AllWeatherRadial에서 적용

--- ObjectInit/ObjectInit/ObjectInit/Tire/AllWeatherRadial.m
+++ ObjectInit/ObjectInit/ObjectInit/Tire/AllWeatherRadial.m
@@ -10,6 +10,15 @@
 
 @implementation AllWeatherRadial
 
+- (id) initWithPressure: (float) p treadDepth: (float) td {
+    if (self = [super initWithPressure: p treadDepth: td]) {
+        rainHandling = 23.7;
+        snowHandling = 42.5;
+    }
+    
+    return (self);
+} // initWithPressure:treadDepth:
+
 - (void) setRainHandling: (float) rh {
     rainHandling = rh;
 } // setRainHandling
