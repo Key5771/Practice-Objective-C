83894fd8dcf882f4b381e5f9033cf55c24405347
key5771 <ksj57715@hanmail.net>
2021-02-16 15:57:47 +0900

    Create Slant6 Class

---
+++ Protocol/Protocol/Protocol/Engine/Slant6.h
@@ -0,0 +1,16 @@
+//
+//  Slant6.h
+//  Protocol
+//
+//  Created by 김기현 on 2021/02/16.
+//
+
+#ifndef Slant6_h
+#define Slant6_h
+
+#import "Engine.h"
+
+@interface Slant6 : Engine
+@end // Slant6
+
+#endif /* Slant6_h */
---
+++ Protocol/Protocol/Protocol/Engine/Slant6.m
@@ -0,0 +1,17 @@
+//
+//  Slant6.m
+//  Protocol
+//
+//  Created by 김기현 on 2021/02/16.
+//
+
+#import <Foundation/Foundation.h>
+#import "Slant6.h"
+
+@implementation Slant6
+
+- (NSString *) description {
+    return @"I am an slant-6. VROOOOOOOM!";
+} // description
+
+@end // Slant6
