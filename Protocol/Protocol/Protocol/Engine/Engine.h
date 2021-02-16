4cf66ff04797ffa09216154cf98294112ece75a3
key5771 <ksj57715@hanmail.net>
2021-02-16 15:41:34 +0900

    Create Engine Class

---
+++ Protocol/Protocol/Protocol/Engine/Engine.h
@@ -0,0 +1,14 @@
+//
+//  Engine.h
+//  Protocol
+//
+//  Created by 김기현 on 2021/02/16.
+//
+
+#ifndef Engine_h
+#define Engine_h
+
+@interface Engine : NSObject
+@end // Engine
+
+#endif /* Engine_h */
---
+++ Protocol/Protocol/Protocol/Engine/Engine.m
@@ -0,0 +1,17 @@
+//
+//  Engine.m
+//  Protocol
+//
+//  Created by 김기현 on 2021/02/16.
+//
+
+#import <Foundation/Foundation.h>
+#import "Engine.h"
+
+@implementation Engine
+
+- (NSString *) description {
+    return @"I am an engine. Vroom!";
+} // description
+
+@end // Engine
