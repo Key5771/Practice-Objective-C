67408cc390b1b2decc38da9a73ae38ea86685e01
key5771 <ksj57715@hanmail.net>
2021-02-16 16:23:55 +0900

    Tire 클래스를 상속받은 AllWeatherRadial, Engine 클래스를 상속받은 Slant6 클래스를 사용하여 원하는 값 출력

--- Protocol/Protocol/Protocol/main.m
+++ Protocol/Protocol/Protocol/main.m
@@ -7,10 +7,33 @@
 
 #import <Foundation/Foundation.h>
 
+#import "Tire.h"
+#import "AllWeatherRadial.h"
+#import "Engine.h"
+#import "Slant6.h"
+#import "Car.h"
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        Car * car = [[Car alloc] init];
+        car.name = @"Herbie";
+        
+        int i;
+        for (i = 0; i < 4; i++) {
+            AllWeatherRadial * tire;
+            tire = [[AllWeatherRadial alloc] init];
+            
+            tire.rainHandling = 20 + i;
+            tire.snowHandling = 28 + i;
+            
+            NSLog(@"the tire's handling is %.f %.f", tire.rainHandling, tire.snowHandling);
+            
+            [car setTire: tire atIndex: i];
+        }
+        
+        car.engine = [[Slant6 alloc] init];
+        
+        [car print];
     }
     return 0;
 }
