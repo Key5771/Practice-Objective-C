40738435e227df6e4a31e20e96e071615b82f097
key5771 <ksj57715@hanmail.net>
2021-02-18 17:18:31 +0900

    Create CategoryTest

---
+++ Working with Objects/WorkingWithObjects/WorkingWithObjects/CategoryTest/SomeObject.h
@@ -0,0 +1,16 @@
+//
+//  SomeObject.h
+//  WorkingWithObjects
+//
+//  Created by 김기현 on 2021/02/18.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SomeObject : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Working with Objects/WorkingWithObjects/WorkingWithObjects/CategoryTest/SomeObject.m
@@ -0,0 +1,12 @@
+//
+//  SomeObject.m
+//  WorkingWithObjects
+//
+//  Created by 김기현 on 2021/02/18.
+//
+
+#import "SomeObject.h"
+
+@implementation SomeObject
+
+@end
